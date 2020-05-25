f273502bd8e97054cf81e0309abf7a25e8f5ca78
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-05-25 11:14:40 +0530

    Refactor. rename test case name

--- src/Leave/leave.test.jsx
+++ src/Leave/leave.test.jsx
@@ -4,7 +4,7 @@ import {render} from '@testing-library/react';
 import LeaveApplication from './leave';
 
 describe("The Leave Application form ",() =>{
-    test('render the empty leave form',() =>{
+    test('render the empty LeaveApplication form',() =>{
         const {getByText,getByLabelText } = render(<LeaveApplication/>);
     });
 });
